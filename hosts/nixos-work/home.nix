a10812bd596ad8377f476c6c6eefa52d847f73e4
debaser36 <n.burkholder@tu-berlin.de>
2025-03-24 14:51:24 +0100

    initial work laptop config

---
+++ hosts/nixos-work/home.nix
@@ -0,0 +1,209 @@
+{config, pkgs,...}:
+#test commen
+{
+	home.username = "nico";
+	home.homeDirectory = "/home/nico";
+	home.packages = with pkgs; [
+
+		isabelle
+		
+		bruno
+		element-desktop
+
+		neofetch
+		nnn
+		#firefox
+		thunderbird
+
+		swaylock
+		swayidle
+		mako
+		waybar
+
+		# archives
+		zip
+		xz
+		unzip
+		p7zip
+
+		#utils
+		jq
+		yq-go
+
+		#network
+		mtr
+		iperf3
+		dnsutils
+		ldns
+		aria2
+		socat
+		nmap
+		ipcalc
+		
+		#misc
+		which
+		tree
+		gnused
+		zstd
+		gnupg
+		libnotify
+		kitty # terminal emulator
+		alacritty
+		keepassxc
+		mako 
+		wl-clipboard
+		shotman
+
+		#nix-related
+		nix-output-monitor
+		
+		#systools
+		xorg.xmodmap
+		sysstat
+		lm_sensors
+		ethtool
+		pciutils
+		usbutils
+		xorg.xrandr
+
+		#gaming
+		steam
+	];
+
+	home.sessionVariables = {
+		WLR_RENDERER_ALLOW_SOFTWARE="1";
+		};
+	
+
+	# GIT
+	programs.git = {
+		enable = true;
+		userName = "debaser36";
+		userEmail = "n.burkholder@tu-berlin.de";	
+	};
+
+	# FIREFOX
+	programs.firefox = {
+		enable = true;
+		profiles."nico" = {
+			#extensions.packages
+		};
+
+	};
+		
+ 	
+	# WAYLAND
+	wayland.windowManager.sway = {
+		checkConfig = false;
+		enable = true;
+		wrapperFeatures.gtk = true;
+		config = rec {
+			modifier = "Mod4";
+			menu = "wofi --show run";
+			bars = [
+				{
+					command = "waybar";
+				}
+			];
+			terminal = "alacritty";
+			window = {
+				#border = 100;
+			};
+			colors = {
+				focused.border = "#6ea5ff";
+				focused.background = "#367ff5";
+				focused.childBorder = "#2878fa";
+				focused.indicator = "#2878fa";
+				focused.text = "#ffffff";
+				unfocused.border = "a1fff7";
+				unfocused.background = "#0061fc";
+				unfocused.childBorder = "#1770ff";
+				unfocused.indicator = "#1df5e3";
+				unfocused.text = "#dcdede";
+			};
+			output."*".bg = "/etc/default_wallpaper.jpg fill";
+			input = {
+				"*" = {xkb_layout = "de";};
+			};
+		};
+		extraConfig = ''
+			output HDMI-A-1 disable
+			bindsym Print			exec shotman -c output
+			bindsym Print+Shift		exec shotman -c region
+			bindsym Print+Shift+Control	exec shotman -c window
+		'';
+		extraOptions = [
+			"--unsupported-gpu"
+			#"--my-next-gpu-wont-be-nvidia"
+		];
+  	};
+
+		programs.waybar = {
+			enable = true;
+			settings = [{
+				"layer"="top";
+				"modules-left"= ["sway/workspaces" "sway/mode"];
+				"modules-center" = ["sway/window"];
+				"modules-right" = [ "clock"];
+				"clock" = {
+					"format-alt"= "{:%a, %d. %b  %H:%M}";
+				};
+			}];
+		};
+	
+
+	# BEVLOED NVIM
+  	programs.neovim = {
+      		enable = true;
+      		defaultEditor = true;
+					withNodeJs = true;
+      		viAlias = true;
+					vimAlias = true;
+      		vimdiffAlias = true;
+					plugins = with pkgs.vimPlugins; [
+						nvim-lspconfig
+						nvim-treesitter.withAllGrammars
+						plenary-nvim
+						gruvbox-material
+						mini-nvim
+					];
+	      	extraConfig = ''
+						set tabstop=2
+						set softtabstop=2
+						set shiftwidth=2
+						set autoindent
+						set number
+						set wildmode=longest,list
+						set cc=80
+						filetype plugin indent on
+						syntax on
+						set clipboard=unnamedplus
+						set mouse=a
+						filetype plugin on
+						set cursorline
+						set ttyfast
+			'';
+		  };
+		
+		 
+		#-------------Bash settings------------
+		programs.bash.enable = true;
+		programs.bash.shellAliases = {
+			ll = "ls -l";
+			la = "ls -A";
+			ls = "ls -A --color=tty";
+		};
+		programs.bash.bashrcExtra = "export PATH=\"$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin\"";
+		programs.kitty.enable = true;
+
+		programs.alacritty.enable = true;
+
+
+		# don't change these Lines!!!
+		home.stateVersion = "24.11";
+		programs.home-manager.enable = true;
+	}
+
+
+
+
