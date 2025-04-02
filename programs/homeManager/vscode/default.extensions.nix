0a8290cba811650707a81772e71eb2fa4a9506e9
debaser36 <n.burkholder@tu-berlin.de>
2025-04-02 14:25:18 +0200

    more extensions

--- programs/homeManager/vscode/default.extensions.nix
+++ programs/homeManager/vscode/default.extensions.nix
@@ -1,6 +1,6 @@
 {pkgs,...}:
 {
-  extensions = with pkgs.vscode-extensions; [
+  extensions = with pkgs.vscode-extensions; with pkgs.vscode-marketplace; [
       	# TypeScript/JavaScript/Node Development
 				esbenp.prettier-vscode            # Code formatting
 				christian-kohler.npm-intellisense # NPM intellisense
@@ -11,17 +11,27 @@
 				bbenoist.nix                      # Nix language syntax highlighting
 				jnoortheen.nix-ide                # Enhanced Nix IDE features
 				arrterian.nix-env-selector        # Nix environment selector
+
+        # Error Lens
+        usernamehw.errorlens              # Useful Error Lensing
+        dsznajder.es7-react-js-snippets   # Snippets for React, Javascript, ...
+        ms-vscode.vscode-typescript-next  # latest Typescript features
+        clinyong.vscode-css-modules       # Better CSS Modules support
+
   ];
   userSettings = {
       "nix.enableLanguageServer" = true;
       "nix.serverPath" = "nil";
-
-      
       "nix.serverSettings" = {
-        "nil" = {
+      "nil" = {
         "formatting" = {
           "command" = [ "nixfmt" ];
         };
+      };
+
+
+      "cssModules.pathAlias" = {
+          "@styles"= "\${workspaceFolder}/src/styles";
       };
     };
   };
