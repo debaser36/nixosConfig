6ba8811bce2feb002e08c1a6015bd12305fda8f6
debaser36 <n.burkholder@tu-berlin.de>
2025-04-02 19:27:30 +0200

    psql added

--- hosts/nixos-work/system/systemPackages.nix
+++ hosts/nixos-work/system/systemPackages.nix
@@ -22,7 +22,27 @@
 	services.xserver.enable = true;
 	services.displayManager.sddm = {
 		enable = true;
-		
+	};
+
+	# psql
+	services.postgresql = {
+		enable = true;
+		ensureDatabases = ["pb_app"];
+		enableTCPIP = true;
+		authentication = pkgs.lib.mkOverride 10 ''
+			#...
+			#type database DBuser origin-address auth-method
+			local all       all     trust
+			# ipv4
+			host  all      all     127.0.0.1/32   trust
+			# ipv6
+			host all       all     ::1/128        trust
+  '';
+		initialScipt = pkgs.writeText "init-sql-script" ''
+				ALTER USER postgres WITH PASSWORD 'postgres';
+				CREATE ROLE nico WITH LOGIN PASSWORT 'postgres';
+				GRANT ALL PRIVILEGES ON DATABASE pb_app TO nico;
+		'';
 	};
 
 }
