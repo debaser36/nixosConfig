339caa3b6f4cb5c1cf23b604862095fe1cfd172c
debaser36 <n.burkholder@tu-berlin.de>
2025-04-02 13:38:25 +0200

    alias for cd backwards

--- programs/homeManager/bash/default.nix
+++ programs/homeManager/bash/default.nix
@@ -5,7 +5,10 @@
 		ll = "ls -l";
 		la = "ls -A";
 		ls = "ls -A --color=tty";
-		'..' = "cd ..";
+		".." = "cd ..";
+		"..." = "cd ../..";
+		"...." = "cd ../../..";
+		"....." = "cd ../../../..";
 	};
 	programs.bash.bashrcExtra = "export PATH=\"$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin\"";
 }
