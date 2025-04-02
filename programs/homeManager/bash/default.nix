e7e72882b6eae98001a922127da93fc692d0c115
debaser36 <n.burkholder@tu-berlin.de>
2025-04-02 13:36:14 +0200

    alias for 'cd ..'

--- programs/homeManager/bash/default.nix
+++ programs/homeManager/bash/default.nix
@@ -5,6 +5,7 @@
 		ll = "ls -l";
 		la = "ls -A";
 		ls = "ls -A --color=tty";
+		'..' = "cd ..";
 	};
 	programs.bash.bashrcExtra = "export PATH=\"$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin\"";
 }
