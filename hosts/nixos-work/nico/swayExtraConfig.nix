b8b14a0ac9d9c62fe54ea00ff4c004553d0f22d2
debaser36 <n.burkholder@tu-berlin.de>
2025-04-17 13:29:08 +0200

    bindsyms for changing tablet input

--- hosts/nixos-work/nico/swayExtraConfig.nix
+++ hosts/nixos-work/nico/swayExtraConfig.nix
@@ -1,4 +1,7 @@
 ''
+      bindsym Shift+T+Left input "1386:890:Wacom_One_by_Wacom_S_Pen" map_to_output "HDMI-A-1"
+      bindsym Shift+T+Right input "1386:890:Wacom_One_by_Wacom_S_Pen" map_to_output "DP-2"
+      bindsym Shift+T+Up input "1386:890:Wacom_One_by_Wacom_S_Pen" map_to_output "eDP-1"
 			bindsym Print			exec shotman -c output
 			bindsym Print+Shift		exec shotman -c region
 			bindsym Print+Shift+Control	exec shotman -c window
