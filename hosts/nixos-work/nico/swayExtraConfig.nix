41299d068022edf6e02c792d34e50ab279de7fdb
debaser36 <n.burkholder@tu-berlin.de>
2025-05-22 15:20:20 +0200

    shotman: automatically copy to clipboard

--- hosts/nixos-work/nico/swayExtraConfig.nix
+++ hosts/nixos-work/nico/swayExtraConfig.nix
@@ -2,8 +2,8 @@
       bindsym Shift+T+Left input "1386:890:Wacom_One_by_Wacom_S_Pen" map_to_output "HDMI-A-1"
       bindsym Shift+T+Right input "1386:890:Wacom_One_by_Wacom_S_Pen" map_to_output "DP-2"
       bindsym Shift+T+Up input "1386:890:Wacom_One_by_Wacom_S_Pen" map_to_output "eDP-1"
-			bindsym Print			exec shotman -c output
-			bindsym Print+Shift		exec shotman -c region
-			bindsym Print+Shift+Control	exec shotman -c window
+			bindsym Print			exec shotman -c output -C
+			bindsym Print+Shift		exec shotman -c region -C
+			bindsym Print+Shift+Control	exec shotman -c window -C
 			bindsym L+K+J exec swaylock
 ''
