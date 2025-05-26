0828061e48f184fddcdb9144df7e9a33dfa663c6
debaser36 <n.burkholder@tu-berlin.de>
2025-05-26 13:40:01 +0200

    correct monitor position calculation

--- hosts/nixos-work/hardware/swayMonitorSettings.nix
+++ hosts/nixos-work/hardware/swayMonitorSettings.nix
@@ -23,14 +23,24 @@
 			rotation= "normal";
 		}
  }:
- let
+let
 	left_mode = "${toString left_monitor.width}x${toString left_monitor.height}@${toString left_monitor.refresh_rate}Hz";
 	center_mode = "${toString center_monitor.width}x${toString center_monitor.height}@${toString center_monitor.refresh_rate}Hz";
 	right_mode = "${toString right_monitor.width}x${toString right_monitor.height}@${toString right_monitor.refresh_rate}Hz";
 
+	# Calculate effective dimensions considering rotation
+	left_effective_width = if left_monitor.rotation == "90" then left_monitor.height else left_monitor.width;
+	left_effective_height = if left_monitor.rotation == "90" then left_monitor.width else left_monitor.height;
+	
+	center_effective_width = if center_monitor.rotation == "90" then center_monitor.height else center_monitor.width;
+	center_effective_height = if center_monitor.rotation == "90" then center_monitor.width else center_monitor.height;
+	
+	right_effective_height = if right_monitor.rotation == "90" then right_monitor.width else right_monitor.height;
+
+	# Position calculations
 	left_position = "0,0";
-	center_position = "${toString left_monitor.width},${toString (left_monitor.height / 5)}";
-	right_position = "${toString (left_monitor.width + center_monitor.width)},${toString (left_monitor.height / 5 - 120)}";
+	center_position = "${toString left_effective_width},${toString ((left_effective_height - center_effective_height) / 2)}";
+	right_position = "${toString (left_effective_width + center_effective_width)},${toString ((left_effective_height - right_effective_height) / 2)}";
 in
  {
 					output.${left_monitor.name} = {
