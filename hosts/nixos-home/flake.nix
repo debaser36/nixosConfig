1fd0b8b57c8d0dbd8fa1658d9efd175a4f6a84dc
debaser36 <n.burkholder@tu-berlin.de>
2025-06-04 18:46:06 +0200

    .

--- hosts/nixos-home/flake.nix
+++ hosts/nixos-home/flake.nix
@@ -2,7 +2,7 @@
 	description = "My very first nixos config flake :))";
 	#author="nico@nixos";
 	inputs={
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		home-manager.url = "github:nix-community/home-manager";
 		home-manager.inputs.nixpkgs.follows = "nixpkgs";
 		nur = {
