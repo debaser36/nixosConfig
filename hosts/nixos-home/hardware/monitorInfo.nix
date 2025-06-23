b5edb9bedab3fe67bd7dd3441cded13c20ed27f0
debaser36 <n.burkholder@tu-berlin.de>
2025-06-23 13:15:20 +0200

    removed unused comments

--- hosts/nixos-home/hardware/monitorInfo.nix
+++ hosts/nixos-home/hardware/monitorInfo.nix
@@ -1,12 +1,4 @@
 {
-  /* left_monitor = {
-        			name= "HDMI-A-1";
-        			width= 1920;
-        			height= 1200;
-        			refresh_rate= 59.950;
-        			rotation= "90";
-    		}; */
-
   center_monitor = {
     name = "DP-2";
     width = 2560;
@@ -14,12 +6,4 @@
     refresh_rate = 59.951;
     rotation = "normal";
   };
-
-  /* right_monitor = {
-        			name= "DP-1";
-        			width= 2560;
-        			height= 1440;
-        			refresh_rate= 59.951;
-        			rotation= "normal";
-    		}; */
 }
