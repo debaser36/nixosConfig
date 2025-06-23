730e6e04fbd9c727fa29f389aef758196fb3294e
debaser36 <n.burkholder@tu-berlin.de>
2025-06-23 13:14:07 +0200

    format

--- hosts/nixos-home/hardware/monitorInfo.nix
+++ hosts/nixos-home/hardware/monitorInfo.nix
@@ -1,11 +1,11 @@
 {
   /* left_monitor = {
-     			name= "HDMI-A-1";
-     			width= 1920;
-     			height= 1200;
-     			refresh_rate= 59.950;
-     			rotation= "90";
-  		}; */
+        			name= "HDMI-A-1";
+        			width= 1920;
+        			height= 1200;
+        			refresh_rate= 59.950;
+        			rotation= "90";
+    		}; */
 
   center_monitor = {
     name = "DP-2";
@@ -16,10 +16,10 @@
   };
 
   /* right_monitor = {
-     			name= "DP-1";
-     			width= 2560;
-     			height= 1440;
-     			refresh_rate= 59.951;
-     			rotation= "normal";
-  		}; */
+        			name= "DP-1";
+        			width= 2560;
+        			height= 1440;
+        			refresh_rate= 59.951;
+        			rotation= "normal";
+    		}; */
 }
