3da81259b9cf6a5f7ca0909b8ff6ee805afd1b14
debaser36 <n.burkholder@tu-berlin.de>
2025-07-16 14:12:44 +0200

    overriding tex/clisp

--- hosts/nixos-work/system/nix_settings.nix
+++ hosts/nixos-work/system/nix_settings.nix
@@ -16,6 +16,17 @@
           flutter = prev.flutter319;
         }
       ) */
+       (final: prev: {
+          clisp = prev.clisp.overrideAttrs (oldAttrs: {
+          version="2.49.92";
+          src = prev.fetchFromGitLab {
+            owner = "gnu-clisp";
+            repo = "clisp";
+            rev = "bf72805c4dace982a6d3399ff4e7f7d5e77ab99a"; # this is actually 2.5
+            hash = "sha256-sQoN2FUg9BPaCgvCF91lFsU/zLja1NrgWsEIr2cPiqo=";
+          };
+        });
+      })
     ];
     config = {
       allowUnfree = true;
