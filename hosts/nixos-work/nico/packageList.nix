c993e6c182de78b5fa343db3cf2bdf3ea0dc5aab
debaser36 <n.burkholder@tu-berlin.de>
2025-09-15 14:20:27 +0200

    python313->314

--- hosts/nixos-work/nico/packageList.nix
+++ hosts/nixos-work/nico/packageList.nix
@@ -15,9 +15,7 @@ with pkgs; [
   git-crypt # Git encryption
   # exercism # Programming exercises platform
   corepack # Node.js package manager manager
-  python313Full # Python interpreter with all modules
-  python313Packages.pip # Python package installer
-  # duckdb # In-process SQL database
+  python314 # Python interpreter with all modules
   nil # Nix language server
 
   # Development Build Tools & Compilers
