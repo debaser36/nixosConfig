04063b8eaba057b15b69d60876ba998e610d29b5
debaser36 <n.burkholder@tu-berlin.de>
2026-02-11 17:05:31 +0100

    trusted localhost IP for keycloak

--- hosts/nixos-oracle/http_https/system_settings.nix
+++ hosts/nixos-oracle/http_https/system_settings.nix
@@ -26,6 +26,8 @@
     enableTCPIP = true;
     authentication = lib.mkOverride 10 ''
       local all all trust
+      host all all 127.0.0.1/32 md5     
+      host all all ::1/128 md5              # IPv6 localhost
       host all all 130.61.234.104/32 md5
       host all all ::/0 md5
     '';
