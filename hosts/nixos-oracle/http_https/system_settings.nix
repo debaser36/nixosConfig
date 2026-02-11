966aded6df0a615fe785b95f6cbaeb3d49684c9f
debaser36 <n.burkholder@tu-berlin.de>
2026-02-11 17:07:22 +0100

    trusted pgadmin instance ip

--- hosts/nixos-oracle/http_https/system_settings.nix
+++ hosts/nixos-oracle/http_https/system_settings.nix
@@ -28,7 +28,7 @@
       local all all trust
       host all all 127.0.0.1/32 md5     
       host all all ::1/128 md5              # IPv6 localhost
-      host all all 130.61.234.104/32 md5
+      host all all 141.23.223.55/32 md5
       host all all ::/0 md5
     '';
   };
