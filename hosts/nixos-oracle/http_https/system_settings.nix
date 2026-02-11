13bded6b4f7a591853b6867d578af606ca882b6e
debaser36 <n.burkholder@tu-berlin.de>
2026-02-11 17:13:50 +0100

    removed open ip settings

--- hosts/nixos-oracle/http_https/system_settings.nix
+++ hosts/nixos-oracle/http_https/system_settings.nix
@@ -29,7 +29,6 @@
       host all all 127.0.0.1/32 md5     
       host all all ::1/128 md5              # IPv6 localhost
       host all all 141.23.223.55/32 md5
-      host all all ::/0 md5
     '';
   };
   services.keycloak = {
