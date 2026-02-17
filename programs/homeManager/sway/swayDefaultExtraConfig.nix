228fa722eef7ff64d3db0d4f70a98d987c5231c6
debaser36 <n.burkholder@tu-berlin.de>
2026-02-17 14:34:58 +0100

    nixfmt

--- programs/homeManager/sway/swayDefaultExtraConfig.nix
+++ programs/homeManager/sway/swayDefaultExtraConfig.nix
@@ -1,10 +1,10 @@
 ''
-  			bindsym Print			exec shotman -c output
-  			bindsym Print+Shift		exec shotman -c region
-  			bindsym Print+Shift+Control	exec shotman -c window
+    			bindsym Print			exec shotman -c output
+    			bindsym Print+Shift		exec shotman -c region
+    			bindsym Print+Shift+Control	exec shotman -c window
 
-				bindsym XF86MonBrightnessUp exec brightnessctl set 1%+
-				bindsym XF86MonBrightnessDown exec brightnessctl set 1%-
-				bindsym XF86MonBrightnessUp+Shift exec brightnessctl set 10%+
-				bindsym XF86MonBrightnessDown+Shift exec brightnessctl set 10%-
+  				bindsym XF86MonBrightnessUp exec brightnessctl set 1%+
+  				bindsym XF86MonBrightnessDown exec brightnessctl set 1%-
+  				bindsym XF86MonBrightnessUp+Shift exec brightnessctl set 10%+
+  				bindsym XF86MonBrightnessDown+Shift exec brightnessctl set 10%-
 ''
