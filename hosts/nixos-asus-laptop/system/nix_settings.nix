620d2f3760c3e87bddfc509e9bffc8103a502521
debaser36 <n.burkholder@tu-berlin.de>
2026-03-27 19:09:02 +1300

    no-url-literals -> lint-url-literals

--- hosts/nixos-asus-laptop/system/nix_settings.nix
+++ hosts/nixos-asus-laptop/system/nix_settings.nix
@@ -5,7 +5,7 @@
 {
   nix = {
     package = pkgs.nixVersions.latest;
-    extraOptions = "experimental-features = nix-command flakes no-url-literals";
+    extraOptions = "experimental-features = nix-command flakes lint-url-literals = fatal";
     settings.download-buffer-size = 524288000;
   };
   virtualisation.waydroid.enable = true;
