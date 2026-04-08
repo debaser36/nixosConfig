34098b31b653a5dd2fac5e11daa2ceac4f9c8bf9
debaser36 <n.burkholder@tu-berlin.de>
2026-04-08 15:42:50 +1200

    feat: added mouse profile to sway

--- hosts/nixos-asus-laptop/hardware/swayInputSettings.nix
+++ hosts/nixos-asus-laptop/hardware/swayInputSettings.nix
@@ -4,4 +4,10 @@
   input."1386:890:Wacom_One_by_Wacom_S_Pen" = {
     map_to_output = initial_output;
   };
+
+  input."12994:102:2.4G_Wireless_Mouse" =  {
+    accel_profile = "flat";
+    drag = "enabled";
+    pointer_accel =  "-0.5"; # this is the default 
+  };
 }
