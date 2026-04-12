c6bce63795cb14f0ab8f717ead282249b32c81f7
debaser36 <n.burkholder@tu-berlin.de>
2026-04-12 15:13:24 +1200

    added hotspot service definition

--- hosts/nixos-asus-laptop/system/networking.nix
+++ hosts/nixos-asus-laptop/system/networking.nix
@@ -8,4 +8,13 @@ _: {
     };
     wireguard.enable = true;
   };
+  services.create_ap = {
+  enable = true;
+  settings = {
+    INTERNET_IFACE = "enp5s0f3u2";
+    WIFI_IFACE = "wlp3s0";
+    SSID = "TetherTest";
+    PASSPHRASE = "Passwort";
+  };
+};
 }
