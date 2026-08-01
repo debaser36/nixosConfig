e08d708664029029f48ff33dccdfd505eda2b951
debaser36 <n.burkholder@tu-berlin.de>
2026-08-02 11:21:39 +1200

    fix: interactive terminal session should now work, if fish is the base terminal (as it should be)

--- programs/homeManager/bash/default.nix
+++ programs/homeManager/bash/default.nix
@@ -1,4 +1,4 @@
-_: {
+{ pkgs, ... }: {
   #-------------Bash settings------------
   programs.bash = {
     enable = true;
@@ -13,6 +13,16 @@ _: {
     };
     bashrcExtra = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin:$HOME/.npm-global/bin"
+    '';
+
+    initExtra = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" \
+        && -z ''${BASH_EXECUTION_STRING} \
+        && -t 1 ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
     '';
   };
 }
